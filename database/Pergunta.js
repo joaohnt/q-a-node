7ba7e25350f9361380ee1476387274d0b82ded1b
joaohnt <jpedrooo.cg@gmail.com>
2025-05-04 08:21:10 -0300

    db

---
+++ database/Pergunta.js
@@ -0,0 +1,15 @@
+const Sequelize = require("sequelize");
+const conn = require("./database");
+
+const Pergunta = conn.define("perguntas", {
+  titulo: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  descricao: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+  },
+});
+
+Pergunta.sync({ force: false }).then(() => {});
--- index.js
+++ index.js
@@ -1,14 +1,40 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const conn = require("./database/database");
+const perguntaModel = require("./database/Pergunta");
+
+// db
+conn
+  .authenticate()
+  .then(() => {
+    console.log("conexão feita com o banco de dados");
+  })
+  .catch((msgErro) => {
+    console.log(msgErro);
+  });
+
+// usar ejs com view engine
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+// isso decofica os dados do form e ler os dados json
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// rotas
 app.get("/", (req, res) => {
   res.render("index", {});
 });
 
 app.get("/perguntar", (req, res) => {
   res.render("perguntar", {});
+});
+
+app.post("/salvarpergunta", (req, res) => {
+  var titulo = req.body.titulo;
+  var descricao = req.body.descricao;
+  res.send("recebido: " + titulo + " " + descricao);
 });
 
 app.listen(8080, () => {
